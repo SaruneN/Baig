5e3b6bcd83d0014ea4f4cacb278250e41c233862
Šarūnė Navickaitė <navickaite.sarune@gmail.com>
2022-11-17 15:05:41 +0200

    Add Driver.java

---
+++ src/test/java/baigiamasis/utils/Driver.java
@@ -0,0 +1,37 @@
+package baigiamasis.utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class Driver {
+
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+
+    public static void setDriver() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(getOptions());
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(8));
+
+    }
+
+    private static ChromeOptions getOptions() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("window-size=2000,3000");
+        options.addArguments("--force-device-scale-factor=0.75");
+        options.addArguments("--incognito");
+//        options.addArguments("--headless");
+        return options;
+    }
+
+    public static void close() {
+        driver.quit();
+    }
+}
