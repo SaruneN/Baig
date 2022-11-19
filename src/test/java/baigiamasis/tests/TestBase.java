974d1b44935ae63ee23b7b010a1e81084cdb0137
Šarūnė Navickaitė <navickaite.sarune@gmail.com>
2022-11-19 17:37:52 +0200

    Add TestListener

--- src/test/java/baigiamasis/tests/TestBase.java
+++ src/test/java/baigiamasis/tests/TestBase.java
@@ -1,9 +1,12 @@
 package baigiamasis.tests;
 
 import baigiamasis.page.LoginPage;
+import baigiamasis.utils.TestListener;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 
+@Listeners(TestListener.class)
 public class TestBase {
 
     @BeforeMethod
---
+++ src/test/java/baigiamasis/utils/TestListener.java
@@ -0,0 +1,49 @@
+package baigiamasis.utils;
+
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
+public class TestListener implements ITestListener {
+    @Override
+    public void onTestFailure(ITestResult result) {
+        takescreenshot("failure");
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        takescreenshot("success");
+    }
+
+    private void takescreenshot(String status) {
+
+        try {
+            TakesScreenshot takesScreenshot = (TakesScreenshot) Driver.getDriver();
+
+            File screenshotRaw = takesScreenshot.getScreenshotAs(OutputType.FILE);
+            LocalDateTime localDateTime = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH_mm_ss_SSS");
+            String dateAndTime = localDateTime.format(formatter);
+
+
+            String directory = "./screenshots/";
+            String fileName = status + "_screenshot_" + dateAndTime + UUID.randomUUID() + ".png";
+
+
+            File screenshotFile = new File(directory + fileName);
+            FileUtils.copyFile(screenshotRaw, screenshotFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
